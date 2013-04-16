21b83a523761380e1844067a71b387e83ace19e2
uglybugger <andrewh@uglybugger.org>
2013-04-17 08:28:57 +1000

    ^E^C.

--- Source/MLAA.Web/WebForm2.aspx.cs
+++ Source/MLAA.Web/WebForm2.aspx.cs
@@ -1,21 +1,11 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 using MLAA.Data.Linq2Sql;
 
 namespace MLAA.Web
 {
-    public partial class WebForm2 : System.Web.UI.Page
+    public partial class WebForm2 : Page
     {
-        protected override void OnLoad(EventArgs e)
-        {
-            base.OnLoad(e);
-        }
-
         public WebForm2ViewModel ViewModel { get; set; }
     }
 
