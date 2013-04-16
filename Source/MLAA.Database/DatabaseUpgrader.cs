abecd1a222115905875e40a7a0ac257df9c3cd57
uglybugger <andrewh@uglybugger.org>
2013-04-16 22:59:23 +1000

    Added DbUp and Hello, world! SQL script so we can see what it does to the database.

---
+++ Source/MLAA.Database/DatabaseUpgrader.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using DbUp;
+
+namespace MLAA.Database
+{
+    public static class DatabaseUpgrader
+    {
+        public static void UpgradeTheWorld(string connectionString)
+        {
+            var upgrader = DeployChanges.To
+                                        .SqlDatabase(connectionString)
+                                        .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                                        .LogToTrace()
+                                        .Build();
+
+            var result = upgrader.PerformUpgrade();
+            if (!result.Successful) throw result.Error;
+        }
+    }
+}
