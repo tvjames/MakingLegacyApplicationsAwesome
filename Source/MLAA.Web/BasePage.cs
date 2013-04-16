b04ac1b97969843c8f3f162f2735cfec5203a347
uglybugger <andrewh@uglybugger.org>
2013-04-17 08:32:28 +1000

    Extracted to own class.

---
+++ Source/MLAA.Web/BasePage.cs
@@ -0,0 +1,12 @@
+﻿namespace MLAA.Web
+{
+    public class BasePage<TViewModel> where TViewModel : class, new()
+    {
+        public BasePage()
+        {
+            ViewModel = new TViewModel();
+        }
+
+        public TViewModel ViewModel { get; set; }
+    }
+}
--- Source/MLAA.Web/WebForm2.aspx.cs
+++ Source/MLAA.Web/WebForm2.aspx.cs
@@ -3,14 +3,4 @@
     public partial class WebForm2 : BasePage<WebForm2ViewModel>
     {
     }
-
-    public class BasePage<TViewModel> where TViewModel : class, new()
-    {
-        public BasePage()
-        {
-            ViewModel = new TViewModel();
-        }
-
-        public TViewModel ViewModel { get; set; }
-    }
 }
