434c38bf1c175f765fae2dccdbe8b3299c3b92dd
uglybugger <andrewh@uglybugger.org>
2013-04-17 08:20:53 +1000

    More clean-up.

--- Source/MLAA.Web/EnrolmentManager.cs
+++ Source/MLAA.Web/EnrolmentManager.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Configuration;
-using System.Data.SqlClient;
+﻿using System.Configuration;
 using System.Linq;
 using MLAA.Data.Linq2Sql;
 
 namespace MLAA.Web
 {
-    /// <summary>
-    ///     This class is where everything about student enrokllments goes. DO NOT PUT ANYTHING ABOUT ENROLMENTS ANYWHERE ELSE
-    ///     OR I WILL SHOUT AT YOU.
-    /// </summary>
-    public  class EnrolmentManager
+    public class EnrolmentManager
     {
-        private readonly DerpUniversityDataContext _context = new DerpUniversityDataContext(ConfigurationManager.ConnectionStrings["DerpUniversityConnectionString"].ConnectionString);
+        private readonly DerpUniversityDataContext _context =
+            new DerpUniversityDataContext(ConfigurationManager.ConnectionStrings["DerpUniversityConnectionString"].ConnectionString);
 
-       
-        public  bool IsEnrolled(int studentId, int subjectId)
+        public bool IsEnrolled(int studentId, int subjectId)
         {
             return _context
                 .StudentSubjectEnrolments
@@ -29,26 +23,15 @@ namespace MLAA.Web
         /// </summary>
         /// <param name="name">Any Part of the first name or last name of the student.</param>
         /// <returns></returns>
-        [Obsolete("Use the non-horrible version, please :)")]
-        public  SqlDataReader SearchStudents(string name)
-        {
-            var sql = "SELECT * FROM Student WHERE LastName LIKE '" + name + "%'";
-            var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DerpUniversityConnectionString"].ConnectionString);
-            connection.Open();
-            var command = new SqlCommand(sql, connection);
-            var result = command.ExecuteReader();
-            return result;
-        }
-
-        public  Student[] SearchStudentsNonHorrible(string name)
+        public Student[] SearchStudentsNonHorrible(string name)
         {
             var context = new DerpUniversityDataContext(ConfigurationManager.ConnectionStrings["DerpUniversityConnectionString"].ConnectionString);
             return context.Students
-                          .Where(s => s.LastName.StartsWith(name)) //FIXME bug? Should this be .Contains(name)?
+                          .Where(s => s.LastName.StartsWith(name)) //FIXME bug? Should this be .Contains(name)? According to the XMLDoc it's a bug...
                           .ToArray();
         }
 
-        public  Subject[] GetStudentEnrolments(int name)
+        public Subject[] GetStudentEnrolments(int name)
         {
             var student = _context.Students.First(s => s.Id == name);
             return student
