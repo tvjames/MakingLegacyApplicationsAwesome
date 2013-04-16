565cc8984802f2125371126ab60f7cefc83c6faf
uglybugger <andrewh@uglybugger.org>
2013-04-17 08:30:32 +1000

    Extracted viewmodel to its own class.

--- Source/MLAA.Web/WebForm2.aspx.cs
+++ Source/MLAA.Web/WebForm2.aspx.cs
@@ -1,27 +1,9 @@
-﻿using System;
-using System.Web.UI;
-using MLAA.Data.Linq2Sql;
+﻿using System.Web.UI;
 
 namespace MLAA.Web
 {
     public partial class WebForm2 : Page
     {
         public WebForm2ViewModel ViewModel { get; set; }
-    }
-
-    public class WebForm2ViewModel
-    {
-        public WebForm2ViewModel()
-        {
-            Students = new EnrolmentManager().SearchStudents("");
-        }
-
-        public Student[] Students { get; set; }
-
-        [Obsolete("This is a dirty hack. Please refactor.")]
-        public Subject[] GetStudentEnrolments(int studentId)
-        {
-            return new EnrolmentManager().GetStudentEnrolments(studentId);
-        }
     }
 }
---
+++ Source/MLAA.Web/WebForm2ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MLAA.Data.Linq2Sql;
+
+namespace MLAA.Web
+{
+    public class WebForm2ViewModel
+    {
+        public WebForm2ViewModel()
+        {
+            Students = new EnrolmentManager().SearchStudents("");
+        }
+
+        public Student[] Students { get; set; }
+
+        [Obsolete("This is a dirty hack. Please refactor.")]
+        public Subject[] GetStudentEnrolments(int studentId)
+        {
+            return new EnrolmentManager().GetStudentEnrolments(studentId);
+        }
+    }
+}
