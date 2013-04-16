697a2015022e3ed5dd6e172909c6939562e1608f
uglybugger <andrewh@uglybugger.org>
2013-04-17 08:37:09 +1000

    Extracted class.

--- Source/MLAA.Web/WebForm1.aspx.cs
+++ Source/MLAA.Web/WebForm1.aspx.cs
@@ -122,19 +122,4 @@ namespace MLAA.Web
             }
         }
     }
-
-    public class WebForm1ViewModel
-    {
-        private readonly EnrolmentManager _enrolmentManager;
-
-        public WebForm1ViewModel(EnrolmentManager enrolmentManager)
-        {
-            _enrolmentManager = enrolmentManager;
-        }
-
-        public bool IsEnrolled(int userId, int subjectId)
-        {
-            return _enrolmentManager.IsEnrolled(userId, subjectId);
-        }
-    }
 }
---
+++ Source/MLAA.Web/WebForm1ViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace MLAA.Web
+{
+    public class WebForm1ViewModel
+    {
+        private readonly EnrolmentManager _enrolmentManager;
+
+        public WebForm1ViewModel(EnrolmentManager enrolmentManager)
+        {
+            _enrolmentManager = enrolmentManager;
+        }
+
+        public bool IsEnrolled(int userId, int subjectId)
+        {
+            return _enrolmentManager.IsEnrolled(userId, subjectId);
+        }
+    }
+}
